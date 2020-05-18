cf0e4e3e5fe78bb4428b674b4ca521aaa7f8e214
Nasir Iqbal <nasirpucit@hotmail.com>
2020-05-18 14:18:33 +0100

    Added Strong Typed Actions for Product

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store, select } from '@ngrx/store';
 import * as fromProduct from '../state/product.reducer';
+import * as productActions from '../state/product.actions';
 
 @Component({
   selector: 'pm-product-list',
@@ -52,10 +53,12 @@ export class ProductListComponent implements OnInit, OnDestroy {
   checkChanged(value: boolean): void {
     // this.displayCode = value;
 
-    this.store.dispatch({
-      type: "TOGGLE_PRODUCT_CODE",
-      payload: value,
-    });
+    // this.store.dispatch({
+    //   type: "TOGGLE_PRODUCT_CODE",
+    //   payload: value,
+    // });
+
+    this.store.dispatch(new productActions.ToggleProductCode(value));
   }
 
   newProduct(): void {
